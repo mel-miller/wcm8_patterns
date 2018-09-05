729ad6ec1bb0858fa737947242a5b375f026ed25
M Miller <miller.2676@osu.edu>
2018-09-04 20:29:33 -0700

    configure molecules

---
+++ components/_patterns/02-molecules/card/card-ctr.md
@@ -0,0 +1,8 @@
+---
+el: ".card-ctr"
+title: "Card with centered content"
+---
+## Card
+_Title and subtext_
+
+A card variation with a background color.
---
+++ components/_patterns/02-molecules/status/status.js
@@ -0,0 +1,23 @@
+(function($) {
+
+  Drupal.behaviors.status = {
+    attach: function (context, settings) {
+
+      $(context).find('.status-box span.status').each(function() {
+        var info = '<div class="icon"><i class="fas fa-info-circle"></i></div>';
+        var warning = '<div class="icon"><i class="fas fa-exclamation-triangle"></i></div>';
+        var error = '<div class="icon"><i class="fas fa-times-circle"></i></div>';
+
+        if($(this).hasClass('status--status')){
+          $(this).prepend(info);
+        } else if ($(this).hasClass('status--warning')) {
+          $(this).prepend(warning);
+        } else if ($(this).hasClass('status--error')) {
+          $(this).prepend(error);
+        }
+      });
+
+    }
+  };
+
+})(jQuery);
