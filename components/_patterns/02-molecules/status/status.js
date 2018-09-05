914d7e32d1cedcccd961215a6c48dd95865ff267
M Miller <miller.2676@osu.edu>
2018-09-05 15:48:12 -0700

    get attached js to work properly

--- components/_patterns/02-molecules/status/status.js
+++ components/_patterns/02-molecules/status/status.js
@@ -1,23 +1,17 @@
 (function($) {
 
-  Drupal.behaviors.status = {
-    attach: function (context, settings) {
+  $('.status-box span.status').each(function() {
+    var info = '<div class="icon"><i class="fas fa-info-circle"></i></div>';
+    var warning = '<div class="icon"><i class="fas fa-exclamation-triangle"></i></div>';
+    var error = '<div class="icon"><i class="fas fa-times-circle"></i></div>';
 
-      $(context).find('.status-box span.status').each(function() {
-        var info = '<div class="icon"><i class="fas fa-info-circle"></i></div>';
-        var warning = '<div class="icon"><i class="fas fa-exclamation-triangle"></i></div>';
-        var error = '<div class="icon"><i class="fas fa-times-circle"></i></div>';
-
-        if($(this).hasClass('status--status')){
-          $(this).prepend(info);
-        } else if ($(this).hasClass('status--warning')) {
-          $(this).prepend(warning);
-        } else if ($(this).hasClass('status--error')) {
-          $(this).prepend(error);
-        }
-      });
-
+    if ($(this).hasClass('status--status')) {
+      $(this).prepend(info);
+    } else if ($(this).hasClass('status--warning')) {
+      $(this).prepend(warning);
+    } else if ($(this).hasClass('status--error')) {
+      $(this).prepend(error);
     }
-  };
+  });
 
 })(jQuery);
