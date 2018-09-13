c6eefe1f8dd6fe2efb2382867e83224bdc140fb9
Melissa Miller <miller.2676@osu.edu>
2018-09-13 14:52:19 -0700

    update gulpfile to allow for other tasks; add 'pl-deploy' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,42 @@
 /* globals require */
 
-// eslint-disable-next-line strict
-'use strict';
+(function () {
+  'use strict';
 
-// General
-var gulp = require('gulp-help')(require('gulp'));
-var localConfig = {};
+  // General
+  var gulp = require('gulp-help')(require('gulp'));
+  var localConfig = {};
 
-try {
-  localConfig = require('./local.gulp-config');
-}
-catch (e) {
-  if (e.code !== 'MODULE_NOT_FOUND') {
-    throw e;
+  try {
+    localConfig = require('./local.gulp-config');
   }
-}
-require('emulsify-gulp')(gulp, localConfig);
+  catch (e) {
+    // Do nothing.
+  }
+  require('emulsify-gulp')(gulp, localConfig);
+
+
+
+  // custom named tasks start here //
+
+  //pl-deploy
+  var ghpages = require('gh-pages');
+
+  gulp.task('pl-deploy', function(){
+
+    //create build directory
+    gulp.src(['./dist/**/*', './pattern-lab/public/**/*'], { base: './'})
+      .pipe(gulp.dest('build'));
+
+    //Publish the build directory to github pages.
+    ghpages.publish('./build', {remote: 'github', message: 'auto-generated commit via pl-deploy'}, function(err){
+      if (err === undefined) {
+        console.log('PL successfully deployed to github!');
+      } else {
+        console.log(err);
+      }
+    });
+  });
+
+
+})();
