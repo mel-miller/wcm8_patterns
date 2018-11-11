07a94a642e019131bffb233615d993922d48adfd
Melissa Miller <miller.2676@osu.edu>
2018-11-11 14:04:23 -0800

    simulate inline callout markup with jquery

--- components/_patterns/02-molecules/callout/callout.js
+++ components/_patterns/02-molecules/callout/callout.js
@@ -1,18 +1,35 @@
-(function ($, Drupal) {
+(function($, Drupal) {
 
   Drupal.behaviors.callout = {
     attach: function(context, settings) {
 
       'use strict';
 
-      $(context).find('.callout').each(function() {
+      $(context).find('span.callout').each(function() {
+        var caution = '<div class="callout-icon"><span class="visually-hidden">Caution</span><i class="fas fa-exclamation"></i></div>';
+        var ux_tip = '<div class="callout-icon"><span class="visually-hidden">UX Tip</span><i class="fas fa-asterisk"></i></div>';
+
+        $(this).wrapInner('<div class="callout-body"></div>');
+
+        if ($(this).hasClass('callout--caution')) {
+          $(this).prepend(caution);
+        } else if ($(this).hasClass('callout--ux-tip')) {
+          $(this).prepend(ux_tip);
+        }
+
+      });
+
+      $(context).find('div.callout').each(function() {
 
         //get the height of the callout div
         var height = $(this).innerHeight();
 
         //if the callout gets "too tall" nest the icon in the corner instead of the whole height
         if (height > 150) {
-          $('.callout-icon', this).css({'height': '3.25em', 'border-radius': '3px 0 5px' });
+          $('.callout-icon', this).css({
+            'height': '3.25em',
+            'border-radius': '3px 0 5px'
+          });
         }
 
       });
