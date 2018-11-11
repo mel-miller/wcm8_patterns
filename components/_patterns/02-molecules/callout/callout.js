87e16052d88708e6c17f00fe940f3acc55f95061
Melissa Miller <miller.2676@osu.edu>
2018-11-11 13:29:31 -0800

    reconfigure markup and styles for callouts

--- components/_patterns/02-molecules/callout/callout.js
+++ components/_patterns/02-molecules/callout/callout.js
@@ -6,19 +6,13 @@
       'use strict';
 
       $(context).find('.callout').each(function() {
-        var caution = '<div class="icon"><span class="visually-hidden">Caution</span><i class="fas fa-exclamation"></i></div>';
-        var ux_tip = '<div class="icon"><span class="visually-hidden">UX Tip</span><i class="fas fa-asterisk"></i></div>';
 
-        if ($(this).hasClass('callout--caution')) {
-          $(this).prepend(caution);
-        } else if ($(this).hasClass('callout--ux-tip')) {
-          $(this).prepend(ux_tip);
-        }
-
-        //add an additional class if the callout is only one line
+        //get the height of the callout div
         var height = $(this).innerHeight();
-        if (height < 51) {
-          $(this).addClass('short');
+
+        //if the callout gets "too tall" nest the icon in the corner instead of the whole height
+        if (height > 150) {
+          $('.callout-icon', this).css({'height': '3.25em', 'border-radius': '3px 0 5px' });
         }
 
       });
