6e92fd3569cab2ad96b27e8daa66d11ca3955d90
Melissa Miller <miller.2676@osu.edu>
2018-12-05 11:57:15 -0800

    style toolset-sections to have full-width backgrounds

---
+++ dist/06-drupal/node/node-toolset/node-toolset.js
@@ -0,0 +1,2 @@
+'use strict';(function(a,b){b.behaviors.toolsets={attach:function attach(b){'use strict';a(b).find('.toolset-section').each(function(){var b=a(this).attr('id');a(this).wrap('<div class="'+('toolset-section-wrapper toolset-section-wrapper--'+b)+'"></div>')})}}})(jQuery,Drupal);
+//# sourceMappingURL=node-toolset.js.map
