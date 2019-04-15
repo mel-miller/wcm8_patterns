0895a80a8f616eaeeec23e6b6b53a6d74237eb45
M Miller <miller.2676@gmail.com>
2019-04-15 15:47:12 -0700

    auto-generated commit via pl-deploy

--- dist/02-molecules/menus/main-menu/main-menu.js
+++ dist/02-molecules/menus/main-menu/main-menu.js
@@ -1,2 +1,2 @@
-'use strict';window.addEventListener('load',function(){new a11yMenubar('main-nav',document,'Main Navigation',hoverintent)});
+'use strict';window.addEventListener('load',function(){new a11yNavbar('main-nav',{ariaLabel:'Main Navigation',breakpointMinWidth:720,mode:'dualAction',responsiveSubmenuToggles:!0})});
 //# sourceMappingURL=main-menu.js.map
